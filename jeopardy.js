4e14fe2cfa6c82f2063870011c0dde918db60358
Jeffrey Ng <jefreykng@hotmail.com>
2023-04-24 17:22:02 -0400

    no randomization feature

--- jeopardy.js
+++ jeopardy.js
@@ -1,47 +1,46 @@
-
 async function getQuestions(){
-const response = await axios.get('http://jservice.io/api/categories?count='+6)
-console.log(response)
-categoryKey=[]
-for (let i =0; i<6; i++) {categoryKey.push(response.data[i].id)}
-allJeopardyQuestions=[]
-for (key of categoryKey){
-const resp = await axios.get('http://jservice.io/api/category?id='+key)
-allJeopardyQuestions.push(resp)}; 
-
-
-container=[]
-for (let i=0; i<6; i++) {
-    let td = document.createElement('td')
-    td.append(response.data[i].title)
-    container.push(td); 
-
-    }
-
-const heading= document.getElementById('heading')
-for (let x of container){
-    heading.append(x)
-}
-
-
-
-ele= document.getElementById('heading')
-
-ele.innerHTML += '<tr id="heading"></tr>'
-
-
-console.log(allJeopardyQuestions)
-for (let i=0; i<5; i++){
-let tbody=document.querySelector('tbody')
-let trs= document.createElement('tr')
- for (let i=0; i<6; i++){
-     let tds= document.createElement('td')
-     trs.append(tds)
+    const response = await axios.get('http://jservice.io/api/categories?count='+6)
+    console.log(response)
+    categoryKey=[]
+    for (let i =0; i<6; i++) {categoryKey.push(response.data[i].id)}
+    allJeopardyQuestions=[]
+    for (key of categoryKey){
+    const resp = await axios.get('http://jservice.io/api/category?id='+key)
+    allJeopardyQuestions.push(resp)}; 
+    
+    
+    container=[]
+    for (let i=0; i<6; i++) {
+        let td = document.createElement('td')
+        td.append(response.data[i].title)
+        container.push(td); 
+    
         }
-tbody.append(trs)}
-
-for (let i=1; i<6; i++){
-$('tr').eq(i).addClass('questions'+[i])}
+    
+    const heading= document.getElementById('heading')
+    for (let x of container){
+        heading.append(x)
+    }
+    
+    
+    
+    ele= document.getElementById('heading')
+    
+    ele.innerHTML += '<tr id="heading"></tr>'
+    
+    
+    console.log(allJeopardyQuestions)
+    for (let i=0; i<5; i++){
+    let tbody=document.querySelector('tbody')
+    let trs= document.createElement('tr')
+     for (let i=0; i<6; i++){
+         let tds= document.createElement('td')
+         trs.append(tds)
+            }
+    tbody.append(trs)}
+    
+    for (let i=1; i<6; i++){
+    $('tr').eq(i).addClass('questions'+[i])}
 
 
 for (let i =0; i<6; i++){
@@ -127,15 +126,107 @@ a: allJeopardyQuestions[4].data.clues[4].answer}
 const cat6q5 = {q: allJeopardyQuestions[5].data.clues[4].question,
 a: allJeopardyQuestions[5].data.clues[4].answer}
 
-let obj = $('.questions1 td').get(0)
-console.log(typeof(obj)) 
-obj.addEventListener('click', function(e){e.preventDefault(); obj.append(cat1q1.q)})
-obj.addEventListener('dblclick', function(e){e.preventDefault(); obj.append(cat1q1.a)})
 
+
+//ROW 1 questions
+let obj = $('.questions1 td').get(0)
+ obj.addEventListener('click', function(e){e.preventDefault(); obj.innerHTML = cat1q1.q})
+obj.addEventListener('dblclick', function(e){e.preventDefault(); obj.innerHTML= "" ; obj.innerHTML =cat1q1.a})
 let obj1 = $('.questions1 td').get(1)
-console.log(typeof(obj1)) 
-obj1.addEventListener('click', function(e){e.preventDefault(); obj1.append(cat2q1.q)})
-obj1.addEventListener('dblclick', function(e){e.preventDefault();obj1.append(cat2q1.a)})
+obj1.addEventListener('click', function(e){e.preventDefault(); obj1.innerHTML = cat2q1.q})
+obj1.addEventListener('dblclick', function(e){e.preventDefault(); obj1.innerHTML = "" ;obj1.innerHTML=cat2q1.a})
+let obj2 = $('.questions1 td').get(2)
+obj2.addEventListener('click', function(e){e.preventDefault(); obj2.innerHTML = cat3q1q1.q})
+obj2.addEventListener('dblclick', function(e){e.preventDefault(); obj2.innerHTML = "" ;obj2.innerHTML=cat3q1.a})
+let obj3 = $('.questions1 td').get(3)
+obj3.addEventListener('click', function(e){e.preventDefault(); obj3.innerHTML = cat4q1.q})
+obj3.addEventListener('dblclick', function(e){e.preventDefault(); obj3.innerHTML = "" ;obj3.innerHTML=cat4q1.a})
+let obj4 = $('.questions1 td').get(4)
+obj4.addEventListener('click', function(e){e.preventDefault(); obj4.innerHTML = cat5q1.q})
+obj4.addEventListener('dblclick', function(e){e.preventDefault(); obj4.innerHTML = "" ;obj4.innerHTML=cat5q1.a})
+let obj5 = $('.questions1 td').get(5)
+obj5.addEventListener('click', function(e){e.preventDefault(); obj5.innerHTML = cat2q1.q})
+obj5.addEventListener('dblclick', function(e){e.preventDefault(); obj5.innerHTML = "" ;obj5.innerHTML=cat6q1.a})
+
+//ROW 2 questions
+let obj6 = $('.questions2 td').get(0)
+obj6.addEventListener('click', function(e){e.preventDefault(); obj6.innerHTML = cat1q2.q})
+obj6.addEventListener('dblclick', function(e){e.preventDefault(); obj6.innerHTML = "" ;obj6.innerHTML=cat2q2.a})
+let obj7 = $('.questions2 td').get(1)
+obj7.addEventListener('click', function(e){e.preventDefault(); obj7.innerHTML = cat2q2.q})
+obj7.addEventListener('dblclick', function(e){e.preventDefault(); obj7.innerHTML = "" ;obj7.innerHTML=cat3q2.a})
+let obj8 = $('.questions2 td').get(2)
+obj8.addEventListener('click', function(e){e.preventDefault(); obj5.innerHTML = cat3q2.q})
+obj8.addEventListener('dblclick', function(e){e.preventDefault(); obj5.innerHTML = "" ;obj8.innerHTML=cat3q2.a})
+let obj9 = $('.questions2 td').get(3)
+obj9.addEventListener('click', function(e){e.preventDefault(); obj9.innerHTML = cat4q2.q})
+obj9.addEventListener('dblclick', function(e){e.preventDefault(); obj9.innerHTML = "" ;obj9.innerHTML=cat4q2.a})
+let obj10 = $('.questions2 td').get(4)
+obj10.addEventListener('click', function(e){e.preventDefault(); obj10.innerHTML = cat5q2.q})
+obj10.addEventListener('dblclick', function(e){e.preventDefault(); obj10.innerHTML = "" ;obj8.innerHTML=cat5q2.a})
+let obj11 = $('.questions2 td').get(5)
+obj11.addEventListener('click', function(e){e.preventDefault(); obj11.innerHTML = cat6q2.q})
+obj11.addEventListener('dblclick', function(e){e.preventDefault(); obj11.innerHTML = "" ;obj11.innerHTML=cat6q2.a})
+
+//ROW 3 QUESTIONS
+let obj12 = $('.questions3 td').get(0)
+obj12.addEventListener('click', function(e){e.preventDefault(); obj12.innerHTML = cat1q3.q})
+obj12.addEventListener('dblclick', function(e){e.preventDefault(); obj12.innerHTML = "" ;obj12.innerHTML=cat1q3.a})
+let obj13 = $('.questions3 td').get(1)
+obj13.addEventListener('click', function(e){e.preventDefault(); obj13.innerHTML = cat1q3.q})
+obj13.addEventListener('dblclick', function(e){e.preventDefault(); obj13.innerHTML = "" ;obj13.innerHTML=cat2q3.a})
+let obj14 = $('.questions3 td').get(2)
+obj14.addEventListener('click', function(e){e.preventDefault(); obj14.innerHTML = cat1q3.q})
+obj14.addEventListener('dblclick', function(e){e.preventDefault(); obj14.innerHTML = "" ;obj14.innerHTML=cat3q3.a})
+let obj15 = $('.questions3 td').get(0)
+obj15.addEventListener('click', function(e){e.preventDefault(); obj15.innerHTML = cat1q3.q})
+obj15.addEventListener('dblclick', function(e){e.preventDefault(); obj15.innerHTML = "" ;obj15.innerHTML=cat4q3.a})
+let obj16 = $('.questions3 td').get(0)
+obj16.addEventListener('click', function(e){e.preventDefault(); obj16.innerHTML = cat1q3.q})
+obj16.addEventListener('dblclick', function(e){e.preventDefault(); obj16.innerHTML = "" ;obj16.innerHTML=cat5q3.a})
+let obj17 = $('.questions3 td').get(0)
+obj17.addEventListener('click', function(e){e.preventDefault(); obj17.innerHTML = cat1q3.q})
+obj17.addEventListener('dblclick', function(e){e.preventDefault(); obj17.innerHTML = "" ;obj17.innerHTML=cat6q3.a})
+
+//ADD ROW 4 QUESTIONS
+let obj18 = $('.questions4 td').get(0)
+obj18.addEventListener('click', function(e){e.preventDefault(); obj18.innerHTML = cat1q4.q})
+obj18.addEventListener('dblclick', function(e){e.preventDefault(); obj18.innerHTML = "" ;obj18.innerHTML=cat6q3.a})
+let obj19 = $('.questions4 td').get(1)
+obj19.addEventListener('click', function(e){e.preventDefault(); obj19.innerHTML = cat2q4.q})
+obj19.addEventListener('dblclick', function(e){e.preventDefault(); obj19.innerHTML = "" ;obj19.innerHTML=cat6q3.a})
+let obj20 = $('.questions4 td').get(2)
+obj20.addEventListener('click', function(e){e.preventDefault(); obj20.innerHTML = cat3q4.q})
+obj20.addEventListener('dblclick', function(e){e.preventDefault(); obj20.innerHTML = "" ;obj20.innerHTML=cat6q3.a})
+let obj21 = $('.questions4 td').get(3)
+obj21.addEventListener('click', function(e){e.preventDefault(); obj21.innerHTML = cat4q4.q})
+obj21.addEventListener('dblclick', function(e){e.preventDefault(); obj21.innerHTML = "" ;obj21.innerHTML=cat6q3.a})
+let obj22 = $('.questions4 td').get(4)
+obj22.addEventListener('click', function(e){e.preventDefault(); obj22.innerHTML = cat5q4.q})
+obj22.addEventListener('dblclick', function(e){e.preventDefault(); obj22.innerHTML = "" ;obj22.innerHTML=cat6q3.a})
+let obj23 = $('.questions4 td').get(5)
+obj23.addEventListener('click', function(e){e.preventDefault(); obj23.innerHTML = cat6q4.q})
+obj23.addEventListener('dblclick', function(e){e.preventDefault(); obj23.innerHTML = "" ;obj23.innerHTML=cat6q3.a})
+
+//ROW 5 QUESTIONS
+let obj24 = $('.questions5 td').get(0)
+obj24.addEventListener('click', function(e){e.preventDefault(); obj24.innerHTML = cat1q5.q})
+obj24.addEventListener('dblclick', function(e){e.preventDefault(); obj24.innerHTML = "" ;obj24.innerHTML=cat1q5.a})
+let obj25 = $('.questions5 td').get(1)
+obj25.addEventListener('click', function(e){e.preventDefault(); obj25.innerHTML = cat2q5.q})
+obj25.addEventListener('dblclick', function(e){e.preventDefault(); obj25.innerHTML = "" ;obj25.innerHTML=cat2q5.a})
+let obj26 = $('.questions5 td').get(2)
+obj26.addEventListener('click', function(e){e.preventDefault(); obj26.innerHTML = cat3q5.q})
+obj26.addEventListener('dblclick', function(e){e.preventDefault(); obj26.innerHTML = "" ;obj26.innerHTML=cat3q5.a})
+let obj27 = $('.questions5 td').get(3)
+obj27.addEventListener('click', function(e){e.preventDefault(); obj27.innerHTML = cat4q5.q})
+obj27.addEventListener('dblclick', function(e){e.preventDefault(); obj27.innerHTML = "" ;obj27.innerHTML=cat4q5.a})
+let obj28 = $('.questions5 td').get(4)
+obj28.addEventListener('click', function(e){e.preventDefault(); obj28.innerHTML = cat5q5.q})
+obj28.addEventListener('dblclick', function(e){e.preventDefault(); obj28.innerHTML = "" ;obj28.innerHTML=cat5q5.a})
+let obj29 = $('.questions5 td').get(5)
+obj29.addEventListener('click', function(e){e.preventDefault(); obj29.innerHTML = cat6q5.q})
+obj29.addEventListener('dblclick', function(e){e.preventDefault(); obj29.innerHTML = "" ;obj29.innerHTML=cat6q5.a})
 }
 
 
